4642978ed7f5bc8f3514bdeef1373f6750da44e3
Sergey234 <mostov.sergey88@gmail.com>
2022-10-27 15:02:06 +0300

    stage0-module3-task6

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -3,6 +3,7 @@ package lang.print.gaps.task6;
 public class PrePostIncrement {
     public static void main(String[] args) {
         int a = 5;
+        a++;
         int b = 7;
         int c = 12;
         int d = 15;
